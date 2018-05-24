d9808a1c951c35ae54d58acbfeac45b243af961b
Karl Nicholas <karl.nicholas@outlook.com>
2018-05-24 13:42:31 -0700

    Worked out fractions for pom.xml.

--- OpinionsEjb/src/main/java/opca/ejb/util/ImprovedNamingStrategy.java
+++ OpinionsEjb/src/main/java/opca/ejb/util/ImprovedNamingStrategy.java
@@ -1,3 +1,4 @@
+
 package opca.ejb.util;
 
 import org.hibernate.boot.model.naming.Identifier;
@@ -42,17 +43,6 @@ public class ImprovedNamingStrategy implements PhysicalNamingStrategy {
 		if (identifier == null || identifier.getText().trim().isEmpty()) {
 			return identifier;
 		}
-/*
-		final StringBuilder buf = new StringBuilder(identifier.getText().replace('.', '_'));
-		for (int i = 1; i < buf.length() - 1; i++) {
-			if (Character.isLowerCase(buf.charAt(i - 1)) && Character.isUpperCase(buf.charAt(i))
-					&& Character.isLowerCase(buf.charAt(i + 1))) {
-				buf.insert(i++, '_');
-			}
-		}
-		String newName = buf.toString().toLowerCase();
-		return Identifier.toIdentifier(newName);
-*/		
 		return Identifier.toIdentifier(identifier.getText().toLowerCase());
 	}
 }
--- OpinionsEjb/src/main/java/opca/service/RestServicesFactory.java
+++ OpinionsEjb/src/main/java/opca/service/RestServicesFactory.java
@@ -1,74 +1,51 @@
 package opca.service;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
+import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.Properties;
 import java.util.logging.Logger;
 
 import service.Client;
 import client.StatutesRsService;
+
 /**
- * This class is a singleton that loads and holds all Role definitions from 
- * the database. Very specific to this particular 
- * application. Should really only be used by UserAccountEJB.
+ * This class is a singleton that loads and holds all Role definitions from the
+ * database. Very specific to this particular application. Should really only be
+ * used by UserAccountEJB.
  * 
  * @author Karl Nicholas
  *
  */
 public class RestServicesFactory {
 	private Logger logger = Logger.getLogger(RestServicesFactory.class.getName());
-	private String statutesRsServiceUrl = "http://localhost:8080/statutesrs/rs/";
-	protected Properties applicationProps;
+	private final String defaultAddress = "http://localhost:8080/statutesrs/rs/";
 
-	public RestServicesFactory() {
-		Properties defaultProps = new Properties();
-		// create and load default properties
-		try ( InputStream in = this.getClass().getResourceAsStream("/defaultProperties.properties") ) {
-			if ( in != null ) defaultProps.load(in);
-			if ( in != null ) in.close();
-		} catch (FileNotFoundException e) {
-			// ignored
-		} catch (IOException e) {
-			throw new RuntimeException(e);
-		}
-		
-		try ( InputStream in = this.getClass().getResourceAsStream("/appProperties.properties") ) {			
-			// create application properties with default
-			applicationProps = new Properties(defaultProps);
-			// now load properties 
-			// from last invocation
-			if ( in != null ) applicationProps.load(in);
-			else applicationProps = defaultProps;
-			if ( in != null ) in.close();
-		} catch (FileNotFoundException e) {
-			// ignored
-		} catch (IOException e) {
-			throw new RuntimeException(e);
+	// private constructor to avoid client applications to use constructor
+	public Client connectStatutesRsService() {
+		try {
+			String s = System.getenv("statutesrsservice");
+			Client statutesRs = null;
+			URL rsLocation;
+			if (s != null)
+				rsLocation = new URL(s);
+			else
+				rsLocation = new URL(defaultAddress);
+			int retryCount = 3;
+			while (retryCount-- > 0) {
+				try {
+					statutesRs = new StatutesRsService(rsLocation).getRsService();
+				} catch (Exception e) {
+					logger.info("new StatutesWSService - Exception");
+					try {
+						Thread.sleep(60000);
+					} catch (InterruptedException e2) {
+						throw new RuntimeException(e2);
+					}
+				}
+				retryCount = 0;
+			}
+			return statutesRs;
+		} catch (MalformedURLException e1) {
+			throw new RuntimeException(e1);
 		}
 	}
-    //private constructor to avoid client applications to use constructor
-    public Client connectStatutesRsService() {
-    	String serviceUrl = applicationProps.getProperty("statutesRsServiceUrl");
-    	if ( serviceUrl == null ) serviceUrl = statutesRsServiceUrl;
-	    Client statutesRs = null;
-	    int retryCount = 3;
-	    while ( retryCount-- > 0 ) {
-	        try {
-	//        	statutesWS = new StatutesWSService(new URL("http://localhost:9080/StatutesWS?wsdl")).getStatutesWSPort();
-	        	statutesRs = new StatutesRsService(new URL(serviceUrl)).getRsService();
-			} catch (Exception e) {
-		        logger.info("new StatutesWSService - Exception");
-		        try {
-					Thread.sleep(60000);
-				} catch (InterruptedException e2) {
-					throw new RuntimeException(e2);
-				}
-			}
-	        retryCount = 0;
-	    }
-	    return statutesRs;
-    }
 }
-
